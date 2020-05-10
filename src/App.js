c79d0b9b3fa87055a6904e266bb6bde80fb99c1d
Paulo Dutra <paulodutra75@gmail.com>
2020-05-10 13:04:57 -0300

    pasta imagens,pages,components criada, e configuração da navagação entre rotas no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { createStore } from "redux";
 import {Provider} from "react-redux";
+import * as page from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-        
+        <BrowserRouter>
+          <Switch>
+            <Route exact path = "/" component = {page.Home} />
+            <Route exact path = "/login" component = {page.Login} />
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
